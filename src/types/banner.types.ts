d7809a3386c963b8dcf788a4b4ef3ba752ac71c2
Neutron35 <steve.lieuron@gmail.com>
2024-10-23 19:44:58 +0200

    feat: added some interface types

---
+++ src/types/banner.types.ts
@@ -0,0 +1,4 @@
+export interface bannerType {
+  title: string;
+  content: string;
+}
---
+++ src/types/gallery.types.ts
@@ -0,0 +1,8 @@
+export interface galleryType {
+  title: string;
+  content: {
+    title: string;
+    tags: string[];
+    image: string;
+  }[];
+}
