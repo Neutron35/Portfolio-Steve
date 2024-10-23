ca373a919edeab2a905d4ca3f48638902259d27c
Neutron35 <steve.lieuron@gmail.com>
2024-10-23 19:42:35 +0200

    fix: modified Header for cleaner code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,10 @@
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from '../ui/navigation-menu';
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { useEffect, useState } from 'react';
 
-import { Button } from '../ui/button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavLink } from 'react-router-dom';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import getTailwindBreakpoints from '@/lib/tailwindBreakpoints';
-import useScreenSize from '@/hooks/useScreenSize';
+import Navbar from '../Navbar';
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const screenSize = useScreenSize();
-  const { tabletBP } = getTailwindBreakpoints();
-
   const [position, setPosition] = useState(window.scrollY);
   const [visible, setVisible] = useState(true);
 
@@ -32,20 +23,6 @@ function Header() {
     };
   }, [position]);
 
-  // Fermer le menu si l'écran est plus large que le breakpoint tablette
-  useEffect(() => {
-    if (screenSize.width >= tabletBP && isOpen) {
-      setIsOpen(false);
-    }
-  }, [screenSize.width, tabletBP, isOpen]);
-
-  const navLinks = [
-    ['Accueil', '/'],
-    ['A propos', '/about'],
-    ['Projets', '/projects'],
-    ['Contact', '/contact'],
-  ];
-
   return (
     <header
       className={`sticky top-0 z-50 w-full bg-black1 transition-transform duration-300 ${
@@ -57,31 +34,7 @@ function Header() {
           <NavigationMenuItem className="py-2">
             <NavLink to="/">LIEURON</NavLink>
           </NavigationMenuItem>
-          <div className="flex w-full justify-end">
-            <Popover open={isOpen} onOpenChange={setIsOpen}>
-              <PopoverTrigger asChild className="md:hidden">
-                <Button className="bg-transparent hover:bg-transparent">
-                  <FontAwesomeIcon icon={faBars} className="text-lg" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="flex w-full list-none flex-col gap-2 bg-black2">
-                {navLinks.map(([title, path]) => (
-                  <NavigationMenuItem key={title} className="nav-link">
-                    <NavLink to={path}>{title}</NavLink>
-                  </NavigationMenuItem>
-                ))}
-              </PopoverContent>
-            </Popover>
-            <div className="hidden gap-1 md:flex">
-              {navLinks.map(([title, path]) => (
-                <NavigationMenuItem key={title}>
-                  <NavLink to={path} className="nav-link">
-                    {title}
-                  </NavLink>
-                </NavigationMenuItem>
-              ))}
-            </div>
-          </div>
+          <Navbar />
         </NavigationMenuList>
       </NavigationMenu>
     </header>
