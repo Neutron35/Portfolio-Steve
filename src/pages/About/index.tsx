fccc0c3c4e9728234e36aa307cbf0665842de33c
Neutron35 <steve.lieuron@gmail.com>
2024-10-23 11:55:24 +0200

    fix: better styling for About

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -8,9 +8,9 @@ function About() {
           Vestibulum accumsan enim at vehicula feugiat.
         </p>
       </div>
-      <div className="flex w-full justify-between rounded-[100px] border-t border-[#1B1B1B] px-8 pt-8">
-        <h3 className="h-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">Parcours</h3>
-        <div className="flex w-1/2 flex-col gap-3">
+      <div className="flex w-full flex-col justify-between gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:px-8 xl:flex-row">
+        <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">Parcours</h3>
+        <div className="flex flex-col gap-3.5 xl:w-1/2">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus leo in orci volutpat sodales.
             Vestibulum accumsan enim at vehicula feugiat.
@@ -21,9 +21,9 @@ function About() {
           </p>
         </div>
       </div>
-      <div className="flex w-full justify-between rounded-[100px] border-t border-[#1B1B1B] px-8 pt-8">
-        <h3 className="h-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">Compétences</h3>
-        <div className="flex w-1/2 flex-col gap-3">
+      <div className="flex w-full flex-col justify-between gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:px-8 xl:flex-row">
+        <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">Compétences</h3>
+        <div className="flex flex-col gap-3 xl:w-1/2">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus leo in orci volutpat sodales.
             Vestibulum accumsan enim at vehicula feugiat.
