85a42238563c77c8fb189097fbcfe3a91cf16d8f
Neutron35 <steve.lieuron@gmail.com>
2024-10-24 12:40:03 +0200

    fix: commentend "available" button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,10 +15,12 @@ function Home() {
       ></img>
       <div className="flex flex-col items-center gap-5 pt-[220px] md:pt-[321px] xl:pt-[288px]">
         <h1>Steve LIEURON</h1>
+        {/*
         <Button className="flex w-fit gap-3 rounded-[118px] bg-[#1C1C1C] px-7 py-3.5">
           <span className="inline-block size-1.5 rounded-[100%] bg-[#4BCF1F]" />
           <p>Disponible</p>
         </Button>
+        */}
         <p className="w-full text-center md:w-[560px]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus leo in orci volutpat sodales.
           Vestibulum accumsan enim at vehicula feugiat.
