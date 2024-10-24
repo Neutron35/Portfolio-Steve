eb82e841e10a549840dec629f540ce79da8c0478
Neutron35 <steve.lieuron@gmail.com>
2024-10-24 12:34:26 +0200

    fix: modified Gallery for Card's integration

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,22 +1,13 @@
+import Card from '../Card';
 import { galleryType } from '@/types/gallery.types';
 
 function Gallery({ title, content }: galleryType) {
   return (
-    <section className="flex w-full flex-col justify-between gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:px-8 xl:flex-row">
+    <section className="flex w-full flex-col items-center justify-between gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:items-start md:px-8">
       <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">{title}</h3>
       <div className="flex flex-col gap-3.5 xl:w-1/2">
         {content.map((item) => (
-          <div key={item.title}>
-            <p>{item.title}</p>
-            <ul>
-              {item.tags.map((tag) => (
-                <li key={tag}>
-                  <p>{tag}</p>
-                </li>
-              ))}
-            </ul>
-            <img src={item.image} />
-          </div>
+          <Card key={item.title} content={item} />
         ))}
       </div>
     </section>
