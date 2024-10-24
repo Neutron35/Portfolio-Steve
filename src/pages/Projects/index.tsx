b4a2f1c9e775b863f1d68fe010fa5df07196aac3
Neutron35 <steve.lieuron@gmail.com>
2024-10-24 12:34:56 +0200

    fix: removed console log

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -6,7 +6,6 @@ import Gallery from '@/components/Gallery';
 function Projects() {
   const { title: bannerTitle, content: bannerContent } = bannerData;
   const { title: galleryTitle, content: galleryContent } = galleryData;
-  console.log(galleryTitle, galleryContent);
   return (
     <div className="flex flex-col gap-24">
       <Banner title={bannerTitle} content={bannerContent} />
