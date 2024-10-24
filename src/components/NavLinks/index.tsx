d82079aa0570f832fb3bd1e963d45d4b6088d7cb
Neutron35 <steve.lieuron@gmail.com>
2024-10-24 11:35:24 +0200

    feat: added NavLinks component

---
+++ src/components/NavLinks/index.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import { NavigationMenuItem } from '../ui/navigation-menu';
+import { navLinksType } from '@/types/navlinks.types';
+import pagesData from '@/data/pagesData';
+
+function NavLinks({ forPopover }: navLinksType) {
+  const filteredPagesData = pagesData.filter((page) => page.path !== 'notfound' && page.path !== '*');
+  let divClassName = '';
+  if (!forPopover) {
+    divClassName = 'hidden gap-1 md:flex';
+  } else {
+    divClassName = 'flex flex-col gap-5';
+  }
+
+  return (
+    <div className={divClassName}>
+      {filteredPagesData.map((page) => (
+        <NavigationMenuItem key={page.title}>
+          <NavLink to={page.path} className="nav-link">
+            {page.title}
+          </NavLink>
+        </NavigationMenuItem>
+      ))}
+    </div>
+  );
+}
+
+export default NavLinks;
