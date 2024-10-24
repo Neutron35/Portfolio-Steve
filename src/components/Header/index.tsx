79ddd52529870a7e77a610ecf0a48e8375ee2ba6
Neutron35 <steve.lieuron@gmail.com>
2024-10-24 11:36:15 +0200

    fix: modified Header for previous commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,16 @@ import { useEffect, useState } from 'react';
 
 import { NavLink } from 'react-router-dom';
 import Navbar from '../Navbar';
+import { Popover } from '../ui/popover';
+import getTailwindBreakpoints from '@/lib/tailwindBreakpoints';
+import useScreenSize from '@/hooks/useScreenSize';
+
+//import Navbar from '../Navbar';
 
 function Header() {
+  const screenSize = useScreenSize();
+  const { tabletBP } = getTailwindBreakpoints();
+  const [isOpen, setIsOpen] = useState(false);
   const [position, setPosition] = useState(window.scrollY);
   const [visible, setVisible] = useState(true);
 
@@ -23,6 +31,13 @@ function Header() {
     };
   }, [position]);
 
+  // Fermer le menu si l'écran est plus large que le breakpoint tablette
+  useEffect(() => {
+    if (screenSize.width >= tabletBP && isOpen) {
+      setIsOpen(false);
+    }
+  }, [screenSize.width, tabletBP, isOpen]);
+
   return (
     <header
       className={`sticky top-0 z-50 w-full bg-black1 transition-transform duration-300 ${
@@ -30,12 +45,14 @@ function Header() {
       }`}
     >
       <NavigationMenu className="mt-5 max-w-none *:w-full">
-        <NavigationMenuList className="justify-between rounded-[100px] border-t border-t-grey1 bg-black2 px-7 py-3.5">
-          <NavigationMenuItem className="py-2">
-            <NavLink to="/">LIEURON</NavLink>
-          </NavigationMenuItem>
-          <Navbar />
-        </NavigationMenuList>
+        <Popover open={isOpen} onOpenChange={setIsOpen}>
+          <NavigationMenuList className="justify-between rounded-[100px] border-t border-t-grey1 bg-black2 px-7 py-3.5">
+            <NavigationMenuItem className="py-2">
+              <NavLink to="/">LIEURON</NavLink>
+            </NavigationMenuItem>
+            <Navbar visible={visible} />
+          </NavigationMenuList>
+        </Popover>
       </NavigationMenu>
     </header>
   );
