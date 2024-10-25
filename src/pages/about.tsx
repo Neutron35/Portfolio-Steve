9c70227c38df645a3444e7e54ed8d8ccf126414c
Neutron35 <steve.lieuron@gmail.com>
2024-10-25 16:41:59 +0200

    fix: better code for About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,10 +4,10 @@ import Banner from '@/components/banner';
 import Section from '@/components/section';
 
 function About() {
-  const { title: bannerTitle, content: bannerContent } = bannerData;
+  const { title, content } = bannerData;
   return (
     <div className="flex flex-col gap-24">
-      <Banner title={bannerTitle} content={bannerContent} />
+      <Banner title={title} content={content} />
       {sectionsData.map((section) => (
         <Section key={section.title} title={section.title} content={section.content} />
       ))}
