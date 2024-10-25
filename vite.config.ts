d5bbdefecc22787ad62217b6a9a102bcae8a6062
Neutron35 <steve.lieuron@gmail.com>
2024-10-25 18:01:23 +0200

    feat: added plugin png to webp

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import MillionLint from '@million/lint';
+import VitePluginWebpCompress from 'vite-plugin-webp-compress';
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), MillionLint.vite()],
+  plugins: [react(), MillionLint.vite(), VitePluginWebpCompress()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
