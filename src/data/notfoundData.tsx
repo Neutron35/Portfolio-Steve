9c93cf94cbe0e8c24cb324c1eb99625ab0bbbb41
Neutron35 <steve.lieuron@gmail.com>
2024-10-25 16:43:07 +0200

    feat: added data and notfound page

---
+++ src/data/notfoundData.tsx
@@ -0,0 +1,6 @@
+export const bannerData = {
+  title: 'Oups !',
+  content: 'La page que vous recherchez semble introuvable.',
+  tag: 'Erreur 404',
+  link: `Retourner à l'accueil`,
+};
--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -1,5 +1,13 @@
+import Banner from '@/components/banner';
+import { bannerData } from '@/data/notfoundData';
+
 function NotFound() {
-  return <div>404</div>;
+  const { title, content, tag, link } = bannerData;
+  return (
+    <div>
+      <Banner title={title} content={content} tag={tag} link={link} />
+    </div>
+  );
 }
 
 export default NotFound;
