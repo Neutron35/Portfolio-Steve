599931d85afdbb2935ba78172fccc6a44b418a9d
Neutron35 <steve.lieuron@gmail.com>
2024-10-25 12:53:08 +0200

    fix: better responsive for Gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -5,7 +5,7 @@ function Gallery({ title, content }: galleryType) {
   return (
     <section className="flex w-full flex-col items-center justify-between gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:items-start md:px-8">
       <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">{title}</h3>
-      <div className="flex flex-col gap-3.5 xl:w-1/2">
+      <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
         {content.map((item) => (
           <Card key={item.title} content={item} />
         ))}
