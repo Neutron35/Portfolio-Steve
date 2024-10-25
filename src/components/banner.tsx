de3b0d83d2e58c95c2f40e5a06bfec4615cbe649
Neutron35 <steve.lieuron@gmail.com>
2024-10-25 16:41:39 +0200

    feat: integrated linkarrow in Banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,10 +1,21 @@
+import { Badge } from './ui/badge';
+import Linkarrow from './linkarrow';
 import { bannerType } from '@/types/banner.types';
 
-function Banner({ title, content }: bannerType) {
+function Banner({ title, content, tag, link }: bannerType) {
   return (
     <section className="flex w-full flex-col items-center gap-5 pt-9 md:pt-24">
+      {tag && (
+        <Badge
+          variant="default"
+          className="justify-center rounded-[100px] border-t-grey1 bg-black2 px-7 py-2.5 text-white"
+        >
+          <h3>{tag}</h3>
+        </Badge>
+      )}
       <h1>{title}</h1>
       <p className="w-full text-center md:w-[464px]">{content}</p>
+      {link && <Linkarrow linkto="/" text={link} />}
     </section>
   );
 }
