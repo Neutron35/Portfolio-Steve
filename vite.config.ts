bfb00dbf4485342fc395a5c0b3a5f016e114a86e
Neutron35 <steve.lieuron@gmail.com>
2024-10-29 09:59:13 +0100

    fix: removed unused packages and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import MillionLint from '@million/lint';
-import VitePluginWebpCompress from 'vite-plugin-webp-compress';
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), MillionLint.vite(), VitePluginWebpCompress()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
