2545a542467a9ffe62af4e473fa33fe9437207f2
Neutron35 <steve.lieuron@gmail.com>
2024-10-29 09:58:43 +0100

    fix: updated card and gallery types for 84c5339

--- src/types/card.types.ts
+++ src/types/card.types.ts
@@ -1,5 +1,12 @@
 export interface cardType {
+  id: number;
   title: string;
   tags: string[];
   image: string;
+  context: string;
+  description: string;
+  issues: string;
+  skills: string[];
+  githubLink?: string;
+  previewLink?: string;
 }
--- src/types/gallery.types.ts
+++ src/types/gallery.types.ts
@@ -1,8 +1,15 @@
 export interface galleryType {
   title: string;
   content: {
+    id: number;
     title: string;
     tags: string[];
     image: string;
+    context: string;
+    description: string;
+    issues: string;
+    skills: string[];
+    githubLink?: string;
+    previewLink?: string;
   }[];
 }
