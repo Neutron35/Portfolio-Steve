d1cbe6ea55354a8b2e533e1e9c619742f4365127
Neutron35 <steve.lieuron@gmail.com>
2024-10-29 09:57:16 +0100

    fix: updated Banner to allow navigation between projects

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,8 +1,19 @@
 import { Badge } from './ui/badge';
 import Linkarrow from './linkarrow';
 import { bannerType } from '@/types/banner.types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { NavLink, useParams } from 'react-router-dom';
+import { projectsData } from '@/data/projectsData.tsx';
 
-function Banner({ title, content, tag, link }: bannerType) {
+function Banner({ title, content, tag, link, allowNav }: bannerType) {
+  const { projectId } = useParams();
+  const numericProjectId = projectId ? parseInt(projectId, 10) : undefined;
+
+  const nextProject = numericProjectId === projectsData.length - 1 ? 0 : numericProjectId + 1;
+
+  const prevProject = numericProjectId === 0 ? projectsData.length - 1 : numericProjectId - 1;
+
   return (
     <section className="flex w-full flex-col items-center gap-5 pt-9 md:pt-24">
       {tag && (
@@ -13,8 +24,20 @@ function Banner({ title, content, tag, link }: bannerType) {
           <h3>{tag}</h3>
         </Badge>
       )}
-      <h1>{title}</h1>
-      <p className="w-full text-center md:w-[464px]">{content}</p>
+      {allowNav ? (
+        <div className="flex w-full items-center justify-between px-7">
+          <NavLink to={`/project/${prevProject}`} className="text-3xl">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </NavLink>
+          <h1>{title}</h1>
+          <NavLink to={`/project/${nextProject}`} className="text-3xl">
+            <FontAwesomeIcon icon={faArrowRight} />
+          </NavLink>
+        </div>
+      ) : (
+        <h1>{title}</h1>
+      )}
+      <p className="w-full text-center md:w-3/5">{content}</p>
       {link && <Linkarrow linkto="/" text={link} />}
     </section>
   );
