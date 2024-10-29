e4b2734fb864ade1933603214eca66d300616772
Neutron35 <steve.lieuron@gmail.com>
2024-10-29 13:54:15 +0100

    fix: better responsive Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/header';
 import Router from './pages/router';
+import Footer from '@/components/footer.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex min-h-screen max-w-[1279px] flex-col items-center px-4 md:px-7 xl:mx-auto">
         <Header />
-        <main className="flex w-full flex-col items-center gap-24 pb-24">
+        <main className="flex w-full flex-col items-center gap-20 pb-24 md:gap-24">
           <Router />
         </main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
