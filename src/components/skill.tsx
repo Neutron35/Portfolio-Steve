2fa205c1c7e327cd1e3335efd7f9714ac2aab736
Neutron35 <steve.lieuron@gmail.com>
2024-10-30 17:32:55 +0100

    feat: added Skill component for skills

---
+++ src/components/skill.tsx
@@ -0,0 +1,46 @@
+import { faCss3, faGit, faGithub, faHtml5, faJs, faNodeJs, faReact, faSass } from '@fortawesome/free-brands-svg-icons';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card.tsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Mapping des noms d'icônes aux icônes FontAwesome
+const iconMapping = {
+  faHtml5: faHtml5,
+  faCss3: faCss3,
+  faJs: faJs,
+  faReact: faReact,
+  faSass: faSass,
+  faNodeJs: faNodeJs,
+  faGit: faGit,
+  faGithub: faGithub,
+};
+
+function Skill({ content }) {
+  return (
+    <div className="grid w-fit grid-cols-1 gap-9 xl:grid-cols-2 xl:items-stretch xl:justify-around">
+      {content.map((skill, index) => (
+        <Card
+          key={index}
+          className="flex w-full flex-col rounded-[24px] border-x-0 border-b-0 border-t border-t-white bg-black2 xl:mx-0"
+        >
+          <CardHeader className="text-center">
+            <CardTitle className="text-lg">{skill.title}</CardTitle>
+          </CardHeader>
+          <CardContent className="mx-auto w-full text-sm text-white md:w-3/5 xl:w-fit">
+            <ul className="flex flex-col gap-2">
+              {skill.text.map((sentence, sentenceIndex) => (
+                <li key={sentenceIndex}>{sentence}</li>
+              ))}
+            </ul>
+          </CardContent>
+          <CardFooter className="mt-auto flex justify-center gap-1">
+            {skill.logos.map((logo, logoIndex) => (
+              <FontAwesomeIcon key={logoIndex} icon={iconMapping[logo]} className="text-3xl text-white" />
+            ))}
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default Skill;
