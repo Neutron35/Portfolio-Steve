fcce0c936cf1e15191866d5a57786cb761ccf305
Neutron35 <steve.lieuron@gmail.com>
2024-10-30 17:33:41 +0100

    fix: updated About page and data for previous commits

--- src/data/aboutData.tsx
+++ src/data/aboutData.tsx
@@ -3,58 +3,91 @@ export const bannerData = {
 };
 
 export const experienceData = {
+  id: 'experience',
   title: 'Parcours',
   content: [
     {
       title: `Formation Développeur Web`,
-      org: `Openclassrooms`,
-      date: `03.2024 / 11.2024`,
+      description: `Openclassrooms - 03.2024 / 11.2024`,
+      projectsList: [
+        `Démarrez votre formation de Développeur Web`,
+        `Créez la page d'accueil d'une agence de voyage avec HTML & CSS`,
+        `Créez une page web dynamique avec JavaScript`,
+        `Débuggez et optimisez un site de photographe`,
+        `Créez une application web de location immobilière avec React`,
+        `Développez le back-end d'un site de notation de livres`,
+        `Planifiez le développement du site de votre client`,
+        `Créez et publiez votre portfolio de développeur`,
+      ],
     },
     {
       title: `Prep'Apprentissage Développeur Web`,
-      org: `Openclassrooms`,
-      date: `04.2022 / 08.2022`,
+      description: `Openclassrooms - 04.2022 / 08.2022`,
+      projectsList: [
+        `Prenez en main votre formation de développeur web`,
+        `Créez la page d'accueil d'une agence de voyage avec HTML & CSS`,
+      ],
     },
   ],
 };
 
 export const skillsData = {
+  id: 'skills',
   title: 'Compétences',
   content: [
     {
       title: `Intégration et développement front-end`,
-      text: `Intégration de contenu respectant les maquettes et directives du projet en utilisant HTML et CSS. Création d'interfaces responsives et interactives optimisées pour différents appareils.`,
+      text: [
+        `Intégration de contenu respectant les maquettes et directives du projet en utilisant HTML et CSS.`,
+        `Création d'interfaces responsives et interactives optimisées pour différents appareils.`,
+      ],
       logos: ['faHtml5', 'faCss3'],
     },
     {
       title: `Gestion de projets et collaboration`,
-      text: `Suivi des tâches via des outils de gestion de projet, rédaction de spécifications techniques et découpage des fonctionnalités en tâches claires et exploitables pour garantir un développement efficace.`,
+      text: [
+        `Suivi des tâches via des outils de gestion de projet.`,
+        `Rédaction de spécifications techniques et découpage des fonctionnalités en tâches claires et exploitables pour garantir un développement efficace.`,
+      ],
       logos: [],
     },
     {
       title: `Programmation JavaScript`,
-      text: `Manipulation d'éléments DOM, gestion des événements utilisateurs et collecte de données via des formulaires en JavaScript. Familiarité avec les opérations asynchrones et gestion d'état côté front.`,
+      text: [
+        `Manipulation d'éléments DOM.`,
+        `Gestion des événements utilisateurs.`,
+        `Collecte de données via des formulaires en JavaScript.`,
+        `Familiarité avec les opérations asynchrones et gestion d'état côté front.`,
+      ],
       logos: ['faJs'],
     },
     {
       title: `Développement d'applications avec React`,
-      text: `Création d'applications SPA en utilisant React pour construire des composants modulaires et performants, gestion de la navigation avec React Router, et implémentation de hooks personnalisés.`,
+      text: [
+        `Création d'applications SPA en utilisant React pour construire des composants modulaires et performants.`,
+        `Gestion de la navigation avec React Router.`,
+        `Implémentation de hooks personnalisés.`,
+      ],
       logos: ['faReact', 'faSass'],
     },
     {
       title: `Back-end et gestion de données`,
-      text: `Implémentation d’opérations CRUD sécurisées, modélisation logique des données et sécurisation des accès pour garantir la confidentialité et l’intégrité des informations. Connaissance des principes d'architecture REST et d'API pour permettre des interactions efficaces entre le front-end et le back-end.`,
+      text: [
+        `Implémentation d’opérations CRUD sécurisées.`,
+        `Modélisation logique des données.`,
+        `Sécurisation des accès pour garantir la confidentialité et l’intégrité des informations.`,
+        `Connaissance des principes d'architecture REST et d'API pour permettre des interactions efficaces entre le front-end et le back-end.`,
+      ],
       logos: ['faNodeJs'],
     },
     {
       title: `Outils et workflows de développement`,
-      text: `Utilisation de Git et GitHub pour le versionnement, configuration d'environnements de développement front-end, et maîtrise des DevTools de Chrome pour le débogage et l’optimisation de performance.`,
+      text: [
+        `Utilisation de Git et GitHub pour le versionnement.`,
+        `Configuration d'environnements de développement front-end.`,
+        `Maîtrise des DevTools de Chrome pour le débogage et l’optimisation de performance.`,
+      ],
       logos: ['faGit', 'faGithub'],
-    },
-    {
-      title: `Sécurité et déploiement`,
-      text: `Implémentation de pratiques sécurisées pour les opérations CRUD et le stockage de données, ainsi que le déploiement de sites statiques en ligne.`,
-      logos: [],
     },
   ],
 };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,58 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCss3, faGit, faGithub, faHtml5, faJs, faNodeJs, faReact, faSass } from '@fortawesome/free-brands-svg-icons';
-import { bannerData, experienceData, skillsData } from '@/data/aboutData';
-
-import Banner from '@/components/banner';
-
-// Définir un type pour les icônes disponibles
-type IconName = 'faHtml5' | 'faCss3' | 'faJs' | 'faReact' | 'faSass' | 'faNodeJs' | 'faGit' | 'faGithub';
+import Banner from '@/components/banner.tsx';
+import { bannerData, sectionsData } from '@/data/aboutData.tsx';
+import Section from '@/components/section.tsx';
 
 function About() {
-  const { title } = bannerData;
-
-  const iconMapping: Record<IconName, any> = {
-    faHtml5: faHtml5,
-    faCss3: faCss3,
-    faJs: faJs,
-    faReact: faReact,
-    faSass: faSass,
-    faNodeJs: faNodeJs,
-    faGit: faGit,
-    faGithub: faGithub,
-  };
-
   return (
     <div className="flex w-full flex-col gap-24">
-      <Banner title={title} />
-
-      <section className="flex w-full flex-col items-center justify-between gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:items-start md:px-8 xl:flex-row">
-        <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">{experienceData.title}</h3>
-        <div className="flex flex-col gap-3.5 xl:w-1/2">
-          {experienceData.content.map((item, index) => (
-            <div key={index}>
-              <p className="font-bold underline">{item.title}</p>
-              <p>{item.org}</p>
-              <p>{item.date}</p>
-            </div>
-          ))}
-        </div>
-      </section>
-      <section className="flex w-full flex-col items-center justify-between gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:items-start md:px-8 xl:flex-row">
-        <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">{skillsData.title}</h3>
-        <div className="flex flex-col gap-8 xl:w-1/2">
-          {skillsData.content.map((item, index) => (
-            <div key={index} className="">
-              <p className="font-bold underline">{item.title}</p>
-              <p>{item.text}</p>
-              <div className="flex justify-center gap-2 pt-2 text-2xl">
-                {item.logos.map((logo, logoIndex) => (
-                  <FontAwesomeIcon key={logoIndex} icon={iconMapping[logo as IconName]} />
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
+      <Banner title={bannerData.title} />
+      {sectionsData.map((section) => (
+        <Section section={section} key={section.id} />
+      ))}
     </div>
   );
 }
