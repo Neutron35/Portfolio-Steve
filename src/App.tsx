b915fee1ef85675886fc48cb9a4a48c24e598454
Neutron35 <steve.lieuron@gmail.com>
2024-10-30 18:20:35 +0100

    feat: custom hook for scrolling top on page change

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './components/header';
 import Router from './pages/router';
 import Footer from '@/components/footer.tsx';
+import ScrollTop from '@/hooks/ScrollTop.tsx';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollTop />
       <div className="flex min-h-screen max-w-[1279px] flex-col items-center px-4 md:px-7 xl:mx-auto">
         <Header />
         <main className="flex w-full flex-col items-center gap-20 pb-24 md:gap-24">
