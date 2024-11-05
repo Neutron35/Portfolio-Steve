eb24702b95dcb1a679d8189d9e9be75af907a668
Neutron35 <steve.lieuron@gmail.com>
2024-11-05 10:10:34 +0100

    fix: some adjustments

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -4,7 +4,6 @@ import { projectsData } from '@/data/projectsData.tsx';
 import Banner from '@/components/banner.tsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import Collapse from '@/components/collapse.tsx';
 
 function Project() {
   const { projectId } = useParams();
@@ -72,9 +71,7 @@ function Project() {
               </div>
               <div className="flex flex-col gap-4">
                 <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">Problématiques</h3>
-                {issues.map((issue, index) => (
-                  <Collapse key={index} title={issue.title} desc={issue.desc} solutions={issue.solutions} />
-                ))}
+                <p>{issues}</p>
               </div>
               <div className="flex flex-col gap-4">
                 <h3 className="size-fit rounded-[100px] border-t border-grey1 bg-black2 px-7 py-2">Compétences</h3>
