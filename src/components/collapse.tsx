52672a5318e31deceb585c70aad429d9dd7048b9
Neutron35 <steve.lieuron@gmail.com>
2024-11-05 13:55:54 +0100

    fix: more Collapse

--- src/components/collapse.tsx
+++ src/components/collapse.tsx
@@ -1,15 +1,36 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { CollapseProps } from '@/types/collapse.types.ts';
 
-function Collapse({ title, desc, solutions }) {
+function Collapse({ title, desc, solutions }: CollapseProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
-      <div>
+    <div className="overflow-hidden">
+      <div className="flex items-center justify-between rounded-[24px] bg-black2 px-7 py-2 text-base">
         <span>{title}</span>
-        <FontAwesomeIcon icon={faChevronDown} onClick={() => setIsOpen(!isOpen)} />
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          onClick={() => setIsOpen(!isOpen)}
+          className={`transition-transform duration-300 ${isOpen ? 'rotate-180' : ''}`}
+        />
+      </div>
+      <div
+        className={`flex origin-top flex-col gap-2 px-3 text-sm transition-all duration-300 ease-in-out ${
+          isOpen ? 'max-h-[500px] py-2 opacity-100' : 'max-h-0 overflow-hidden py-0 opacity-0'
+        }`}
+      >
+        <ul className="flex flex-col gap-1 border-l-4 border-destructive px-4">
+          {desc.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <ul className="flex flex-col gap-1 border-l-4 border-green-500 px-4">
+          {solutions.map((solution, index) => (
+            <li key={index}>{solution}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
