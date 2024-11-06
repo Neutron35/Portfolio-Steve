227380be433ce6596df4dacbdceb607d1b17079b
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 11:02:48 +0100

    fix: ref.current in a variable

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -15,13 +15,15 @@ const useIntersectionObserver = (threshold: number = 0.1) => {
       { threshold }
     );
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, [threshold]);
