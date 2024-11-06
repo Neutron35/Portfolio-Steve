66e715f7c02bd3f0c30c357080a624c68b987353
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 16:04:47 +0100

    fix: renamed to avoid confusion

--- src/components/aboutSection.tsx
+++ src/components/aboutSection.tsx
@@ -2,23 +2,23 @@ import Timeline from '@/components/timeline.tsx';
 import Skills from '@/components/skills.tsx';
 import { ExperienceContent, SectionType, SkillsContent } from '@/types/section.types.ts';
 
-function Section({ section }: { section: SectionType }) {
+function AboutSection({ section }: { section: SectionType }) {
   let content = <></>;
 
   const isValidContent = (item: any, key: string) => key in item;
 
-  if (section.id === 'experience' && section.content.every(item => isValidContent(item, 'description'))) {
+  if (section.id === 'experience' && section.content.every((item) => isValidContent(item, 'description'))) {
     content = <Timeline content={section.content as ExperienceContent[]} />;
-  } else if (section.id === 'skills' && section.content.every(item => isValidContent(item, 'text'))) {
+  } else if (section.id === 'skills' && section.content.every((item) => isValidContent(item, 'text'))) {
     content = <Skills content={section.content as SkillsContent[]} />;
   }
 
   return (
     <section className="flex w-full flex-col items-center gap-9 rounded-[100px] border-t border-[#1B1B1B] px-4 pt-8 md:px-8">
-      <h3 className="size-fit rounded-[100px] border-t bg-black2 px-7 py-2 md:mr-auto">{section.title}</h3>
+      <h3 className="title3 size-fit rounded-[100px] border-t bg-black2 px-7 py-2 md:mr-auto">{section.title}</h3>
       {content}
     </section>
   );
 }
 
-export default Section;
+export default AboutSection;
