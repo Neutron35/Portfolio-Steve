4babbe7cab0b7296dc56916c7b05fa8e8db95f31
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 11:06:43 +0100

    fix: added a function for conversion logic

--- src/lib/tailwindBreakpoints.ts
+++ src/lib/tailwindBreakpoints.ts
@@ -1,8 +1,11 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
+const getBreakpoint = (screen: keyof typeof defaultTheme.screens): number => 
+  Number(defaultTheme.screens[screen].replace(/\D/g, ''));
+
 const getTailwindBreakpoints = () => {
-  const tabletBP = Number(defaultTheme.screens.md.replace(/\D/g, ''));
-  const desktopBP = Number(defaultTheme.screens.xl.replace(/\D/g, ''));
+  const tabletBP = getBreakpoint('md');
+  const desktopBP = getBreakpoint('xl');
 
   return { tabletBP, desktopBP };
 };
