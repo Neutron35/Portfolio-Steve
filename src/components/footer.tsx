c4c8d893b5f5be100bb931bf3228fc4d16fdedfc
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 12:38:19 +0100

    fix: better lists in Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,24 @@
 import { Link, NavLink } from 'react-router-dom';
-import { NavigationMenu } from '@/components/ui/navigation-menu.tsx';
-import { NavigationMenuItem } from '@radix-ui/react-navigation-menu';
 import NavLinks from '@/components/navlinks.tsx';
 import NavSocials from '@/components/navsocials.tsx';
+import { NavigationMenu } from '@/components/ui/navigation-menu.tsx';
 
 function Footer() {
   return (
     <footer className="flex w-full flex-col gap-5 pb-9">
-      <NavigationMenu className="flex max-w-none list-none flex-col items-center gap-6 xl:flex-row xl:justify-between xl:gap-0">
-        <NavigationMenuItem>
+      <div className="flex max-w-none list-none flex-col items-center gap-6 xl:flex-row xl:justify-between xl:gap-0">
+        <div>
           <NavLink to="/" className="font-bold">
             LIEURON
           </NavLink>
-        </NavigationMenuItem>
-        <NavLinks position="bottom" />
-        <NavSocials />
-      </NavigationMenu>
+        </div>
+        <NavigationMenu>
+          <NavLinks position="bottom" />
+        </NavigationMenu>
+        <NavigationMenu>
+          <NavSocials />
+        </NavigationMenu>
+      </div>
       <div className="alt-style flex flex-col justify-between gap-6 border-t-[3px] border-[#1B1B1B] pt-5 text-sm md:flex-row">
         <div>Créé par Steve LIEURON</div>
         <div>
--- src/components/navsocials.tsx
+++ src/components/navsocials.tsx
@@ -1,10 +1,10 @@
-import { NavigationMenuItem } from '@/components/ui/navigation-menu.tsx';
+import { NavigationMenuItem, NavigationMenuList } from '@/components/ui/navigation-menu.tsx';
 import { socialsData } from '@/data/socialsData.tsx';
 import { Link } from 'react-router-dom';
 
 function NavSocials() {
   return (
-    <div className="flex justify-end gap-5">
+    <NavigationMenuList className="flex justify-end gap-4">
       {socialsData.map(({ title, link, element }) => (
         <NavigationMenuItem key={title} className="text-2xl">
           <Link to={link} target="_blank" rel="noopener noreferrer" aria-label={`Aller sur la page ${title} de Steve`}>
@@ -12,7 +12,7 @@ function NavSocials() {
           </Link>
         </NavigationMenuItem>
       ))}
-    </div>
+    </NavigationMenuList>
   );
 }
 
