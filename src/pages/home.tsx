2cb9293fb74b49a0571a1027a10916ca9b861b14
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 11:11:16 +0100

    fix: created function for animations

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,8 @@ import LinkArrow from '@/components/linkarrow.tsx';
 function Home() {
   const { ref, isVisible } = useIntersectionObserver();
 
+  const animationClasses = (baseClass: string) => `${baseClass} ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`;
+
   return (
     <section className="w-full pt-9 md:pt-16">
       <img
@@ -16,14 +18,10 @@ function Home() {
         ref={ref}
         className={`flex flex-col items-center gap-5 pt-[220px] md:pt-[321px] xl:pt-[288px] ${isVisible ? 'animate-scroll' : 'opacity-0'}`}
       >
-        <h1
-          className={`transition-opacity delay-[400ms] duration-700 ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`}
-        >
+        <h1 className={`transition-opacity delay-[400ms] duration-700 ${animationClasses('')}`}>
           Steve LIEURON
         </h1>
-        <p
-          className={`w-full text-center transition-opacity delay-[800ms] duration-[800ms] ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`}
-        >
+        <p className={`w-full text-center transition-opacity delay-[800ms] duration-[800ms] ${animationClasses('')}`}>
           Développeur web passionné, j'aime créer des expériences utilisateur intuitives et performantes. Formé aux
           technologies modernes et fort de projets concrets, je m'engage à concevoir des solutions élégantes et
           efficaces, en affinant sans cesse mes compétences.
@@ -31,7 +29,7 @@ function Home() {
         <LinkArrow
           linkto="/about"
           text="En savoir plus"
-          className={`transition-opacity delay-1000 duration-[800ms] ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`}
+          className={`transition-opacity delay-1000 duration-[800ms] ${animationClasses('')}`}
         />
       </div>
     </section>
