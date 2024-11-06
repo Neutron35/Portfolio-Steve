8561536b3108a436715c48743433893a58f7cd28
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 16:11:04 +0100

    fix: forgot to push with 66e715f

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,13 @@
 import Banner from '@/components/banner.tsx';
 import { bannerData, sectionsData } from '@/data/aboutData.tsx';
-import Section from '@/components/section.tsx';
+import AboutSection from '@/components/aboutSection.tsx';
 
 function About() {
   return (
     <div className="flex w-full flex-col gap-20 md:gap-24">
       <Banner title={bannerData.title} />
       {sectionsData.map((section) => (
-        <Section section={section} key={section.id} />
+        <AboutSection section={section} key={section.id} />
       ))}
     </div>
   );
