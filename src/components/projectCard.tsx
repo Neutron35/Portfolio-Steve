82e2610bdf93a69b95a58f60edecdf6f12231b8b
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 10:50:34 +0100

    fix: conditional classes are now in const

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -10,6 +10,10 @@ function ProjectCard({ content }: CardProps) {
   const [hover, setHover] = useState(false);
   const { id, title, tags, image } = content;
 
+  const hoverClass = hover ? 'scale-[1.07]' : '';
+  const buttonClass = `transition-transform ${hover ? 'bg-white text-[#1c1c1c]' : ''} h-fit rounded-[100px] border px-3.5 py-1.5 hover:bg-white`;
+  const iconClass = `transition-transform ${hover ? 'rotate-[-30deg]' : ''}`;
+
   return (
     <NavLink to={`/project/${id}`}>
       <article
@@ -18,7 +22,7 @@ function ProjectCard({ content }: CardProps) {
         className="flex flex-col gap-3.5 rounded-[24px] bg-black2 pb-5"
       >
         <div className="h-[400px] w-full overflow-hidden rounded-[24px] md:h-[600px]">
-          <img src={image} alt={title} className={`transition-transform ${hover && 'scale-[1.07]'}`} />
+          <img src={image} alt={title} className={`transition-transform ${hoverClass}`} />
         </div>
         <div className="flex items-start justify-center gap-2 px-5 md:items-end">
           <div className="flex w-full flex-col items-start gap-1">
@@ -35,10 +39,8 @@ function ProjectCard({ content }: CardProps) {
               ))}
             </div>
           </div>
-          <Button
-            className={`transition-transform ${hover && 'bg-white text-[#1c1c1c]'} h-fit rounded-[100px] border px-3.5 py-1.5 hover:bg-white`}
-          >
-            <FontAwesomeIcon icon={faArrowRight} className={`transition-transform ${hover && 'rotate-[-30deg]'}`} />
+          <Button className={buttonClass}>
+            <FontAwesomeIcon icon={faArrowRight} className={iconClass} />
           </Button>
         </div>
       </article>
