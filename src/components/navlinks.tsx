d0ea2eecb80a29080e2f945734dc467cb6524d55
Neutron35 <steve.lieuron@gmail.com>
2024-11-06 10:41:53 +0100

    fix: optimized classNames

--- src/components/navlinks.tsx
+++ src/components/navlinks.tsx
@@ -5,17 +5,15 @@ import pagesData from '@/data/pagesData';
 
 function NavLinks({ position }: navLinksType) {
   const filteredPagesData = pagesData.slice(0, pagesData.length - 3);
-  let divClassName = '';
-  switch (position) {
-    case 'top':
-      divClassName = 'hidden gap-1 md:flex';
-      break;
-    case 'popover':
-      divClassName = 'flex flex-col gap-5';
-      break;
-    case 'bottom':
-      divClassName = 'flex flex-col gap-5 md:flex-row justify-center items-center gap-1';
-  }
+
+  const classNames: Record<string, string> = {
+    top: 'hidden gap-1 md:flex',
+    popover: 'flex flex-col gap-5',
+    bottom: 'flex flex-col gap-1 md:flex-row justify-center items-center gap-1',
+  };
+
+  const divClassName = classNames[position] || '';
+
   return (
     <div className={divClassName}>
       {filteredPagesData.map((page) => (
