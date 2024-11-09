cee32da0ac152f72f7b2df06075e40bd0bb48b5f
Neutron35 <steve.lieuron@gmail.com>
2024-11-09 16:03:26 +0100

    fix: useless flexs

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <ScrollTop />
-      <div className="flex min-h-screen flex-col items-center px-4 md:px-7 xl:mx-auto xl:max-w-[1153px]">
+      <div className="min-h-screen px-4 md:px-7 xl:mx-auto xl:max-w-[1153px]">
         <Header />
-        <main className="flex w-full flex-col items-center gap-20 pb-24 md:gap-24">
+        <main className="w-full pb-24">
           <Router />
         </main>
         <Footer />
